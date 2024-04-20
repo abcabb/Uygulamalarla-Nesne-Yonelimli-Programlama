498df6ddc4e19e6600b92341e8b1f3ae964b7c14
semi <semi1907@outlook.com>
2024-04-20 16:21:56 +0300

    Session 5.6

--- Cpp/vehicle/main.cpp
+++ Cpp/vehicle/main.cpp
@@ -5,12 +5,36 @@ using std::string;
 class Vehicle{
 private:    
     string manufacturer;
+    int year;
+    string color;
 public:
-    string getManufacturer(){
-        return manufacturer;
+    Vehicle(){
+        std::cout<<"An Vehicle Instance has been created.";
     }
-    void setManufacturer(string value){
-        manufacturer = value;
+
+    Vehicle(string manufacturer, int year, string color) : Vehicle(){
+        this->manufacturer = manufacturer;
+        this->year = year;
+        this->color = color;
+    }
+
+    string getManufacturer(){
+        return this->manufacturer;
+    }
+    void setManufacturer(string manufacturer){
+        this->manufacturer = manufacturer;
+    }
+    int getYear(){
+        return this->year;
+    }
+    void setYear(int year){
+        this->year = year;
+    }
+    string getColor(){
+        return this->color;
+    }
+    void setColor(string color){
+        this->color = color;
     }
 };
 
@@ -18,9 +42,9 @@ int main(){
     std::cout <<"Hello World\n";
 
     Vehicle item1 = Vehicle();
-
     item1.setManufacturer("TOGG");
-
-    std::cout <<"The manufacturer is "<<item1.getManufacturer(); 
+    std::cout <<"The manufacturer is "<<item1.getManufacturer() << std::endl; 
     
+    Vehicle item2 = Vehicle("Scoda", 2019, "White");
+    std::cout<<item2.getManufacturer();
 }
